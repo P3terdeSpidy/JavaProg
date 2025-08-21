eda27f7483e8d814a6866aa85ad3d01afa6343e2
P3terdeSpidy <buggytheclown004@gmail.com>
2025-08-21 13:51:46 +0800

    Update HelloWorld.java

--- 01-Hello World/HelloWorld.java
+++ 01-Hello World/HelloWorld.java
@@ -8,5 +8,11 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Hello, World");
+        System.out.println("My name is Phone Myat Thu");
+            System.out.println("Course Name- OBJECT ORIENTED PROGRAMMING IN JAVA I");
+            System.out.println(Course Code-CSC200 A");
+            System.out.println(lECTURE NAME-Sathish Sarma");
+            
+            
     }
 }
